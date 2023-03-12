0196bbbfdccff8fc2cc9658c19e78317d39398b8
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-03-12 22:05:49 +0500

    fmi

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,10 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    idx=0
+    while len(data)>idx:
+        if data[idx]==max(data):
+            break
+        idx+=1
+    return idx
+print(find_max_index([6, 8, 7, 4, 0]))
