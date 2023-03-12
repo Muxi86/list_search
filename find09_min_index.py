dd08cdf92dac0c54fabdb52219e7da862eb4e862
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-03-12 22:34:17 +0500

    fmi2

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,11 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    idx=0
+    while len(data)>idx:
+        if data[idx]==min(data):
+            break
+        idx+=1
+    return idx
+print(find_min_index([12, 2, 5, 2, 7, 9, 1]))
 
